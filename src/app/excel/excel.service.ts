097ee65e0189392d6804ceeb1e45862f0a8c6c56
JV\Zhassulan.Tokbayev <zhassulan.tokbayev@tele2.com>
2019-10-09 14:48:36 +0600

    NCP-22 Format date time in export

--- src/app/excel/excel.service.ts
+++ src/app/excel/excel.service.ts
@@ -12,12 +12,30 @@ export class ExcelService {
     }
 
     save(data) {
+        this.processMills(data);
         const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
         const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
         const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
         const blob: Blob = new Blob([excelBuffer], {type: EXCEL_TYPE});
         //this.fileSaverService.save(blob, 'export_' + new Date().toDateString() + '_' + new Date().toTimeString() + EXCEL_EXTENSION);
         this.fileSaverService.save(blob, 'export' + EXCEL_EXTENSION);
+    }
+
+    processMills(data)  {
+        let options = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            timezone: 'UTC',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        };
+        //alert( date.toLocaleString("ru", options) ); // среда, 31 декабря 2014 г. н.э. 12:30:00
+        data.forEach(item => {
+            item.creationDate = new Date(item.creationDate).toLocaleString("ru", options).replace(',','');
+            item.distributeDate = new Date(item.distributeDate).toLocaleString("ru", options).replace(',','');
+        })
     }
 
 }
