c97e5ba227205a2077d301529e7d20ffcb8d7935
JV\Zhassulan.Tokbayev <zhassulan.tokbayev@tele2.kz>
2020-04-23 16:38:25 +0600

    d

--- src/app/payment/info/info.component.ts
+++ src/app/payment/info/info.component.ts
@@ -1,6 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {NcpPayment} from '../../payments/model/ncp-payment';
-import {PaymentStatusRuPipe} from '../../payments/payment-status-ru-pipe';
 import {Payment} from '../model/payment';
 
 @Component({
