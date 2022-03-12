abf6e7d18007752c422aa94281115f37de862f9f
Zhassulan Tokbayev <Zhassulan.Tokbayev@tele2.kz>
2022-03-12 14:05:54 +0600

    In progress

--- src/app/mobipay/mobipay-repository.ts
+++ src/app/mobipay/mobipay-repository.ts
@@ -6,43 +6,45 @@ import {catchError} from 'rxjs/operators';
 import {Payment} from '../payment/model/payment';
 import {LimitsUpdateResponse} from './model/limits-update-response';
 import {HttpErrHandler} from '../http-err-handler';
+import {headers} from '../settings';
 
 const API_URL = environment.apiUrl;
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MobipayRepository {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    clients() {
-        return this.http.get<Client []>(`${API_URL}/mobipay/clients`).pipe(catchError(HttpErrHandler.handleError));
-    }
+  clients() {
+    return this.http.get<Client []>(`${API_URL}/mobipay/clients`).pipe(catchError(HttpErrHandler.handleError));
+  }
 
-    partners(paymentId) {
-        const params = new HttpParams()
-            .set('paymentId', paymentId);
-        return this.http.get<Client []>(`${API_URL}/mobipay/partners`, {params}).pipe(catchError(HttpErrHandler.handleError));
-    }
+  partners(paymentId) {
+    const params = new HttpParams()
+      .set('paymentId', paymentId);
+    return this.http.get<Client []>(`${API_URL}/mobipay/partners`, {params}).pipe(catchError(HttpErrHandler.handleError));
+  }
 
-    change(id, isMobipay) {
-        const params = new HttpParams()
-            .set('paymentId', id)
-            .set('isMobipay', isMobipay);
-        return this.http.post<Client []>(`${API_URL}/mobipay/change`, {params}).pipe(catchError(HttpErrHandler.handleError));
-    }
+  change(id, isMobipay) {
+    const params = new HttpParams()
+      .set('paymentId', id)
+      .set('isMobipay', isMobipay);
+    return this.http.post<Client []>(`${API_URL}/mobipay/change`, {params}).pipe(catchError(HttpErrHandler.handleError));
+  }
 
-    distribute(id, cancel, partnerCode) {
-        const params = new HttpParams()
-            .set('cancel', cancel)
-            .set('partnerCode', partnerCode);
-        return this.http.post<Payment>(`${API_URL}/mobipay/distribute/${id}`, null, {params}).pipe(catchError(HttpErrHandler.handleError));
-    }
+  distribute(id, cancel, partnerCode) {
+    const params = new HttpParams()
+      .set('cancel', cancel)
+      .set('partnerCode', partnerCode);
+    return this.http.post<Payment>(`${API_URL}/mobipay/distribute/${id}`, null, {params}).pipe(catchError(HttpErrHandler.handleError));
+  }
 
-    updateLimits(formData: FormData) {
-        return this.http.post<LimitsUpdateResponse []>(`${API_URL}/mobipay/limits`, formData).pipe(catchError(HttpErrHandler.handleError));
-    }
+  updateLimits(formData: FormData) {
+    return this.http.post<LimitsUpdateResponse []>(`${API_URL}/mobipay/limits`, formData).pipe(catchError(HttpErrHandler.handleError));
+  }
+
 
 }
--- src/app/payments/payments-table/payments-table.component.ts
+++ src/app/payments/payments-table/payments-table.component.ts
@@ -16,207 +16,208 @@ import {MobipayRepository} from '../../mobipay/mobipay-repository';
 import {Message} from '../../message';
 
 @Component({
-    selector: 'app-payments-table',
-    templateUrl: './payments-table.component.html',
-    styleUrls: ['./payments-table.component.scss']
+  selector: 'app-payments-table',
+  templateUrl: './payments-table.component.html',
+  styleUrls: ['./payments-table.component.scss']
 })
 export class PaymentsTableComponent implements OnInit, OnDestroy {
 
-    displayedColumns = [
-        'ID',
-        'nameSender',
-        'sum',
-        'rnnSender',
-        'paymentDetails',
-        'statusRu',
-        'managedBy',
-        'account',
-        'knp',
-        'select',
-        'rowMenu'];
-    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
-    dataSource = new MatTableDataSource<Payment>();
-    paginatorResultsLength = 0;
-    pageSize = 30;
-    pageSizeOptions: number[] = [50, 100, 150, 250, 300];
-    PaymentStatus = PaymentStatus;
-    @Input() dateRangeComponent;
-    @Input() selection;
-    private subscription: Subscription;
+  displayedColumns = [
+    'ID',
+    'nameSender',
+    'sum',
+    'rnnSender',
+    'paymentDetails',
+    'statusRu',
+    'managedBy',
+    'account',
+    'knp',
+    'select',
+    'rowMenu'];
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  dataSource = new MatTableDataSource<Payment>();
+  paginatorResultsLength = 0;
+  pageSize = 30;
+  pageSizeOptions: number[] = [50, 100, 150, 250, 300];
+  PaymentStatus = PaymentStatus;
+  @Input() dateRangeComponent;
+  @Input() selection;
+  private subscription: Subscription;
 
-    constructor(private payDataService: PaymentRepository,
-                private appService: AppService,
-                private notifService: NotificationsService,
-                private router: Router,
-                private dialogService: DlgService,
-                private excelService: ExcelService,
-                private payService: PaymentService,
-                private mobipayDataService: MobipayRepository) {
+  constructor(private payDataService: PaymentRepository,
+              private appService: AppService,
+              private notifService: NotificationsService,
+              private router: Router,
+              private dialogService: DlgService,
+              private excelService: ExcelService,
+              private payService: PaymentService,
+              private mobipayDataService: MobipayRepository,
+              private mobipayService: MobipayService) {
+  }
+
+  ngOnInit(): void {
+    this.setPaginator();
+    this.loadData();
+  }
+
+  setPaginator() {
+    this.paginatorResultsLength = this.dataSource.data.length;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+  }
+
+  initStatusRu(payments) {
+    payments.forEach(payment => this.setStatusRu(payment));
+  }
+
+  setStatusRu(payment) {
+    payment.statusRu = PaymentStatusRu[payment.status];
+  }
+
+  loadData() {
+    if (isDevMode()) {
+      this.dateRangeComponent.start = '2019-12-31T00:00:00.000';
+      this.dateRangeComponent.end = '2019-12-31T23:59:59.999';
     }
-
-    ngOnInit(): void {
-        this.setPaginator();
-        this.loadData();
-    }
-
-    setPaginator() {
-        this.paginatorResultsLength = this.dataSource.data.length;
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-        this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    }
-
-    initStatusRu(payments) {
-        payments.forEach(payment => this.setStatusRu(payment));
-    }
-
-    setStatusRu(payment) {
-        payment.statusRu = PaymentStatusRu[payment.status];
-    }
-
-    loadData() {
-        if (isDevMode()) {
-            this.dateRangeComponent.start = '2019-12-31T00:00:00.000';
-            this.dateRangeComponent.end = '2019-12-31T23:59:59.999';
-        }
-        this.appService.setProgress(true);
-        this.subscription = this.payDataService.range(this.dateRangeComponent.start, this.dateRangeComponent.end).subscribe(
-            data => {
-                this.initStatusRu(data);
-                this.dataSource.data = data;
-            },
-            error => {
-                this.appService.setProgress(false);
-                this.notifService.error(error.message);
-            },
-            () => this.appService.setProgress(false));
-    }
-
-    loadFileData() {
-        this.appService.setProgress(true);
-        this.dataSource.data = [];
-        this.payDataService.json().subscribe(data => {
-                this.dataSource.data = data;
-            }, error => {
-                this.notifService.error(error.message);
-                this.appService.setProgress(false);
-            },
-            () => this.appService.setProgress(false));
-    }
-
-    isAllSelected() {
-        const numSelected = this.selection.selected.length;
-        const numRows = this.dataSource.data.length;
-        return numSelected === numRows;
-    }
-
-    masterToggle() {
-        this.isAllSelected() ? this.selection.clear() :
-            this.selection.selected.length > 0 ? this.selection.clear() : this.dataSource.filteredData.forEach(row => this.selection.select(row));
-    }
-
-    menuOnRowOpenPayment(paymentRow) {
-        this.router.navigate(['payments/' + paymentRow.id]);
-    }
-
-    menuOnRowMobipay(paymentRow) {
-        this.appService.setProgress(true);
-        this.mobipayDataService.change(paymentRow.id, true).subscribe(
-            data => {
-                this.notifService.info(Message.OK.MOBIPAY_CHANGED);
-            },
-            error => {
-                this.notifService.error(error.message);
-                this.appService.setProgress(false);
-            },
-            () => this.appService.setProgress(false));
-    }
-
-    menuOnRowDeleteTransit(payment) {
-        this.dialogService.clear();
-        this.dialogService.title = 'Удаление с тразитного счёта';
-        this.dialogService.openDialog();
-        this.transitDel(payment.id);
-    }
-
-    menuOnRowTransit(payment) {
-        this.dialogService.clear();
-        this.dialogService.title = 'Перевод на транзитный счёт';
-        this.dialogService.openDialog();
-        this.transit(payment.id);
-    }
-
-    transit(id) {
-        this.appService.setProgress(true);
-        this.payDataService.transit(id).subscribe(
-            data => {
-                let payment = this.dataSource.data.find(x => x.id == id);
-                payment.status = data.status;
-                payment.statusRu = PaymentStatusRu[payment.status];
-                this.dialogService.addItem(`ID ${id} OK - TRANSIT_PDOC_ID ${data.transitPdocNumId}`);
-            },
-            error => {
-                this.dialogService.addItem(`ID ${id} Ошибка - ${error.message}`);
-                this.appService.setProgress(false);
-            },
-            () => this.appService.setProgress(false));
-    }
-
-    transitDel(id) {
-        this.appService.setProgress(true);
-        this.payDataService.transitDel(id).subscribe(
-            data => {
-                let payment = this.dataSource.data.find(x => x.id === id);
-                payment.status = data.status;
-                payment.statusRu = PaymentStatusRu[payment.status];
-                this.dialogService.addItem(`ID ${id} OK - TRANSIT_PDOC_ID ${data.transitPdocNumId}`);
-            },
-            error => {
-                this.dialogService.addItem(`ID ${id} Ошибка - ${error}`);
-                this.appService.setProgress(false);
-            },
-            () => this.appService.setProgress(false));
-    }
-
-    onRowClicked(paymentRow) {
-        //нельзя использовать, т.к. не работает меню строки!
-        //this.menuOnRowOpenPayment(paymentRow);
-    }
-
-    export() {
-        this.selection.selected.length > 0 ? this.excelService.save(this.selection.selected) : this.excelService.save(this.dataSource.data);
-    }
-
-    transitSelected() {
-        if (this.selection.selected.length > 0) {
-            this.dialogService.clear();
-            this.dialogService.title = 'Перевод на транзитный счёт';
-            this.dialogService.openDialog();
-            this.selection.selected.forEach(payment => this.transit(payment.id));
-        }
-    }
-
-    ngOnDestroy(): void {
+    this.appService.setProgress(true);
+    this.subscription = this.payDataService.range(this.dateRangeComponent.start, this.dateRangeComponent.end).subscribe(
+      data => {
+        this.initStatusRu(data);
+        this.dataSource.data = data;
+      },
+      error => {
         this.appService.setProgress(false);
-        if (this.subscription) {
-          this.subscription.unsubscribe();
-        }
-    }
+        this.notifService.error(error.message);
+      },
+      () => this.appService.setProgress(false));
+  }
 
-    canTransit(row) {
-        this.payService.setPayment(row);
-        return this.payService.canTransit();
-    }
+  loadFileData() {
+    this.appService.setProgress(true);
+    this.dataSource.data = [];
+    this.payDataService.json().subscribe(data => {
+        this.dataSource.data = data;
+      }, error => {
+        this.notifService.error(error.message);
+        this.appService.setProgress(false);
+      },
+      () => this.appService.setProgress(false));
+  }
 
-    canDelTransit(row) {
-        this.payService.setPayment(row);
-        return this.payService.canDelTransit();
-    }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
 
-    canMoveMobipay(row) {
-      return this.payService.canMoveMobipay(row);
+  masterToggle() {
+    this.isAllSelected() ? this.selection.clear() :
+      this.selection.selected.length > 0 ? this.selection.clear() : this.dataSource.filteredData.forEach(row => this.selection.select(row));
+  }
+
+  menuOnRowOpenPayment(paymentRow) {
+    this.router.navigate(['payments/' + paymentRow.id]);
+  }
+
+  menuOnRowMobipay(paymentRow) {
+    this.appService.setProgress(true);
+    this.mobipayDataService.change(paymentRow.id, true).subscribe(
+      data => {
+        this.notifService.info(Message.OK.MOBIPAY_CHANGED);
+      },
+      error => {
+        this.notifService.error(error.message);
+        this.appService.setProgress(false);
+      },
+      () => this.appService.setProgress(false));
+  }
+
+  menuOnRowDeleteTransit(payment) {
+    this.dialogService.clear();
+    this.dialogService.title = 'Удаление с тразитного счёта';
+    this.dialogService.openDialog();
+    this.transitDel(payment.id);
+  }
+
+  menuOnRowTransit(payment) {
+    this.dialogService.clear();
+    this.dialogService.title = 'Перевод на транзитный счёт';
+    this.dialogService.openDialog();
+    this.transit(payment.id);
+  }
+
+  transit(id) {
+    this.appService.setProgress(true);
+    this.payDataService.transit(id).subscribe(
+      data => {
+        let payment = this.dataSource.data.find(x => x.id == id);
+        payment.status = data.status;
+        payment.statusRu = PaymentStatusRu[payment.status];
+        this.dialogService.addItem(`ID ${id} OK - TRANSIT_PDOC_ID ${data.transitPdocNumId}`);
+      },
+      error => {
+        this.dialogService.addItem(`ID ${id} Ошибка - ${error.message}`);
+        this.appService.setProgress(false);
+      },
+      () => this.appService.setProgress(false));
+  }
+
+  transitDel(id) {
+    this.appService.setProgress(true);
+    this.payDataService.transitDel(id).subscribe(
+      data => {
+        let payment = this.dataSource.data.find(x => x.id === id);
+        payment.status = data.status;
+        payment.statusRu = PaymentStatusRu[payment.status];
+        this.dialogService.addItem(`ID ${id} OK - TRANSIT_PDOC_ID ${data.transitPdocNumId}`);
+      },
+      error => {
+        this.dialogService.addItem(`ID ${id} Ошибка - ${error}`);
+        this.appService.setProgress(false);
+      },
+      () => this.appService.setProgress(false));
+  }
+
+  onRowClicked(paymentRow) {
+    //нельзя использовать, т.к. не работает меню строки!
+    //this.menuOnRowOpenPayment(paymentRow);
+  }
+
+  export() {
+    this.selection.selected.length > 0 ? this.excelService.save(this.selection.selected) : this.excelService.save(this.dataSource.data);
+  }
+
+  transitSelected() {
+    if (this.selection.selected.length > 0) {
+      this.dialogService.clear();
+      this.dialogService.title = 'Перевод на транзитный счёт';
+      this.dialogService.openDialog();
+      this.selection.selected.forEach(payment => this.transit(payment.id));
     }
+  }
+
+  ngOnDestroy(): void {
+    this.appService.setProgress(false);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  canTransit(row) {
+    this.payService.setPayment(row);
+    return this.payService.canTransit();
+  }
+
+  canDelTransit(row) {
+    this.payService.setPayment(row);
+    return this.payService.canDelTransit();
+  }
+
+  canMoveMobipay(row) {
+    return this.payService.canMoveMobipay(row);
+  }
 
   menuOnRowMoveMobipay(payment: Payment) {
     this.dialogService.clear();
@@ -226,6 +227,9 @@ export class PaymentsTableComponent implements OnInit, OnDestroy {
   }
 
   moveMobipay(payment: Payment) {
-      this.mobipayDataService.change(payment.id, this.payService.isMobipay(payment));
+    this.mobipayService.isMobipay(payment.profileId).subscribe(next: data => {
+        this.mobipayDataService.change(payment.id, data);
+      },
+      error => this.notifService.error(error));
   }
 }
